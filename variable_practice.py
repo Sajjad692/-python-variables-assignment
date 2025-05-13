1a2305b53b14995eca14fa7aad8454b61eb49512
Sajjad Ahmad <sajibaba05@gmail.com>
2025-05-13 18:41:23 +0500

    Add files via upload

---
+++ variable_practice.py
@@ -0,0 +1,9 @@
+name= "Arham" # data type string so we must yse qutation
+age= 13 # data type integer
+course= "Basic Python"
+batch= 1
+
+student_name="Rahim Mir"
+student_age= 11
+student_roll_no=678
+
